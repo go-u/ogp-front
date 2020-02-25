be06d7e4c865656e1175227dfdd13982db055e86
go-u <53477091+go-u@users.noreply.github.com>
2020-02-25 12:34:45 +0900

    Separate header component to keep layout file simple

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,57 +1,7 @@
 <template>
   <q-layout view="hHh lpR fFf">
 
-    <q-header class="bg-grey-9 text-white" height-hint="98">
-      <q-toolbar class="base-container">
-        <q-toolbar-title>
-          <q-avatar v-if="$store.state.auth.authState === 'AUTH_STATE_LOGIN'" class="bg-white q-mr-xs" style="padding: unset;" @click="left=!left">
-            <img :src="$store.state.auth.user.avatar" style="border: solid black 1px"/>
-          </q-avatar>
-
-          <q-btn :icon="$store.state.auth.authState === 'AUTH_STATE_LOGIN' ? undefined : matBookmarkBorder" label="OGP GAL" flat class="text-amber-4 text-weight-bolder" style="letter-spacing: 1px" size="lg" dense :to="{ name: 'top' }"/>
-        </q-toolbar-title>
-        <q-tabs v-if="!$q.platform.is.mobile" inline-label align="right">
-          <q-route-tab :icon="matStars" :class="$route.name==='top' && !$route.query.order ? 'show_indicator' : ''" :to="{ name: 'top', query: { order: 'popular' } }" label="人気順"/>
-          <q-route-tab :icon="matFiberNew" :to="{ name: 'top', query: { order: 'newer' } }" label="新着順" />
-          <q-route-tab :icon="matBookmark" :to="{ name: 'bookmarks' }" label="ブックマーク" />
-        </q-tabs>
-
-        <q-btn-dropdown v-if="$q.platform.is.mobile && $store.state.auth.authState === 'AUTH_STATE_LOGIN'" stretch flat dense label="カテゴリー">
-          <q-list>
-            <q-item exact clickable v-close-popup tabindex="0" :class="$route.name==='top' && !$route.query.order ? 'text-blue' : ''" :to="{ name: 'top', query: { order: 'popular' } }">
-              <q-item-section avatar>
-                <q-avatar :icon="matStars"/>
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>人気順</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item exact clickable v-close-popup tabindex="0" :to="{ name: 'top', query: { order: 'newer' } }">
-              <q-item-section avatar>
-                <q-avatar :icon="matFiberNew"/>
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>新着順</q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item clickable v-close-popup tabindex="0" :to="{ name: 'bookmarks' }">
-              <q-item-section avatar>
-                <q-avatar :icon="matBookmark"/>
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>ブックマーク</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-btn-dropdown>
-
-        <q-btn v-if="$store.state.auth.authState !== 'AUTH_STATE_LOGIN'" id="auth-btn" label="ログイン/登録" color="amber-4" class="text-black" :to="{ name: 'auth' }"/>
-        <div v-if="!$q.platform.is.mobile" id="ribbon-margin"/>
-      </q-toolbar>
-      <q-ribbon v-if="!$q.platform.is.mobile" class="github-text text-weight-bolder" position="top-right" type="corner" color="white" background-color="black" style="font-size: 12px">
-        <a href="https://github.com/go-u/ogp-index" target="_blank" style="text-decoration: none;border-top: 1px solid white;border-bottom: 1px solid white;padding: 4px 0" class="q-ml-sm">Fork me on GITHUB</a>
-      </q-ribbon>
-    </q-header>
+    <site-header v-on:left-drawer="left=!left"/>
 
     <q-drawer v-if="$store.state.auth.authState === 'AUTH_STATE_LOGIN'" v-model="left" side="left" overlay behavior="mobile" bordered>
       <drawer-content/>
@@ -69,18 +19,16 @@
 </template>
 
 <script>
-import { matBookmark, matBookmarkBorder, matStars, matFiberNew } from '@quasar/extras/material-icons'
-import { mapState } from 'vuex'
 import { Plugins } from '@capacitor/core'
 const { SplashScreen } = Plugins
 import DrawerContent from '../components/drawer/DrawerContent'
+import SiteHeader from '../components/header/SiteHeader'
 
 export default {
   name: 'MainLayout',
-  components: { DrawerContent },
+  components: { SiteHeader, DrawerContent },
   data () {
     return {
-      text: '',
       left: false
     }
   },
@@ -94,15 +42,6 @@ export default {
     hideSplash: function () {
       SplashScreen.hide() // アプリのsplash screenを解除
     }
-  },
-  computed: {
-    ...mapState({
-      // icons
-      matBookmark: () => matBookmark,
-      matBookmarkBorder: () => matBookmarkBorder,
-      matFiberNew: () => matFiberNew,
-      matStars: () => matStars
-    })
   }
 }
 </script>
@@ -111,10 +50,4 @@ export default {
 .base-container
   max-width: 1200px
   margin: 0 auto
-
-#ribbon-margin
-  margin-right: 110px
-
-.show_indicator > div
-  opacity: 1!important
 </style>
